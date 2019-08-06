391a353ad6a45e377d36eda45c15d6f7eb4f7df4
Eugene Belov <eugene.beloff@gmail.com>
2019-08-06 12:10:37 +0300

    rename and move components

---
+++ src/dashboard/engagements/EngagementsList/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+const EngagementsList: FC = () => {
+
+    return (
+        <div>EngagementsList</div>
+    )
+}
+
+export default EngagementsList;
---
+++ src/dashboard/engagements/SearchFilter/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+const SearchFilter: FC = () => {
+
+    return (
+        <div>SearchFilter</div>
+    )
+}
+
+export default SearchFilter;
